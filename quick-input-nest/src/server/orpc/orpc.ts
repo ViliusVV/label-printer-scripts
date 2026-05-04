e34d440ece8da8c55fa0110401b36c8a239fa939
Vilius Valinskis <vilius.valinsk@gmail.com>
2026-05-04 18:43:52 +0300

    orpc display err

--- quick-input-nest/src/server/orpc/orpc.ts
+++ quick-input-nest/src/server/orpc/orpc.ts
@@ -5,5 +5,16 @@ export type ORPCContext = {
   inputs: InputStorageService;
 };
 
-export const base = os.$context<ORPCContext>();
+export const base = os.$context<ORPCContext>().use(async ({ next }) => {
+  try {
+    return await next();
+  } catch (err) {
+    if (err instanceof ORPCError) throw err;
+    throw new ORPCError("BAD_REQUEST", {
+      message: err instanceof Error ? err.message : "Unexpected error",
+      cause: err,
+    });
+  }
+});
+
 export { ORPCError };
